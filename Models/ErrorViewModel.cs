207546ab5db6795b79d408147cba5d1d9e2015d7
Torrejonccio <matiast.r@hotmail.com>
2025-10-16 14:27:27 -0300

    Cambio de App Service a Static App

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace PortafolioTorrejon.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
